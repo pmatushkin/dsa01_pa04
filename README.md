72ab3a4c8cc227d7a2a02baf0d9382fa7f7e4f84
pmatushkin <pmatushkin@gmail.com>
2016-07-17 11:46:57 -0400

    feat: Pass Problem 2: Take as Much Gold as Possible

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Problems
 Apply the dynamic programming technique to solve various computational problems:
 * [`/src/primitive_calculator/PrimitiveCalculator.java`](src/primitive_calculator/PrimitiveCalculator.java): Problem 1, Primitive Calculator, computes the minimum number of operations needed to obtain the number *n* starting from the number 1;
-* [`/src/knapsack/Knapsack.java`](src/knapsack/Knapsack.java): Problem 2, Take as Much Gold as Possible, takes as much gold as possible into your bag;
+* [`/src/knapsack/Knapsack.java`](src/knapsack/Knapsack.java): Problem 2, Take as Much Gold as Possible, takes as much gold as possible into your bag (PASSED);
 * [`/src/edit_distance/EditDistance.java`](src/edit_distance/EditDistance.java): Problem 3, Compute the Edit Distance Between Two Strings, implements the algorithm for computing the edit distance between two strings;
 * [`/src/placing_parentheses/PlacingParentheses.java`](src/placing_parentheses/PlacingParentheses.java): Problem 4, Maximize the Value of an Arithmetic Expression, finds the maximum value of an arithmetic expression by specifying the order of applying its arithmetic operations using additional parentheses;
 * [`/src/lcs3/LCS3.java`](src/lcs3/LCS3.java): Advanced Problem 5, Longest Common Subsequence of Three Sequences, computes the length of a longest common subsequence of three sequences.
--- src/knapsack/Knapsack.java
+++ src/knapsack/Knapsack.java
@@ -3,13 +3,45 @@ import java.util.*;
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
         //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+
+        // the number of available items
+        int n = w.length;
+
+        // the matrix of all the possible value solutions
+        int value[][] = new int[W + 1][n + 1];
+
+        // initialize the value matrix
+        for (int i = 0; i <= W; i++) {
+            value[i][0] = 0;
         }
-        return result;
+        for (int i = 0; i <= n; i++) {
+            value[0][i] = 0;
+        }
+
+        // loop through the available items
+        for (int i = 1; i <= n; i++) {
+            // read the current item
+            int wi = w[i - 1];
+
+            // loop through the possible weights
+            for (int j = 1; j <= W; j++) {
+                // assign the initial value(w, i)
+                value[j][i] = value[j][i - 1];
+
+                // check if the current item can be used
+                // that is, the current state of knapsack can fit the current item
+                if (wi <= j) {
+                    int val = value[j - wi][i - 1] + wi;
+
+                    // decide whether to use the current item
+                    if (value[j][i] < val) {
+                        value[j][i] = val;
+                    }
+                }
+            }
+        }
+
+        return value[W][n];
     }
 
     public static void main(String[] args) {
